53ecb1a3f5cfdd678161d12feb554a159aae89b5
Marc Cruz <MarcACruz916@gmail.com>
2024-10-17 19:04:53 -0700

    Added Homework 1 .cu code

--- GPU_src/sgemm.cu
+++ GPU_src/sgemm.cu
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<sys/time.h>
 
+#define BLOCK_SIZE 16
+
 #define CHECK(call){ \
     const cudaError_t cuda_ret = call; \
     if (cuda_ret != cudaSuccess){ \
@@ -91,7 +93,7 @@ void basicSgemm_d_1thread1element(int m, int k, int n, const float *A_h, const f
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMalloc: %f s\n", endTime - startTime);
+    printf("    cudaMalloc:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (2) Copy data to device memory
@@ -101,19 +103,19 @@ void basicSgemm_d_1thread1element(int m, int k, int n, const float *A_h, const f
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (3) Call kernel to launch a grid of threads to perform the computation on GPU
-    dim3 gridDim(16, 16, 1);
-    dim3 blockDim(ceil((float)m / blockDim.x), ceil((float)n / blockDim.y));
+    dim3 blockDim(BLOCK_SIZE, BLOCK_SIZE); 
+    dim3 gridDim((n + blockDim.x - 1) / blockDim.x, (m + blockDim.y - 1) / blockDim.y); 
 
     startTime = myCPUTimer();
     matrixMulKernel_1thread1element<<<gridDim, blockDim>>>(m, k, n, A_d, B_d, C_d);
     CHECK(cudaDeviceSynchronize());
     endTime = myCPUTimer();
 
-    printf("    matrixMulKernel_1thread1element<<<(%d, %d, %d),(%d, %d, %d)>>>: %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
+    printf("    matrixMulKernel_1thread1element<<<(%d, %d, %d),(%d, %d, %d)>>>:                   %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
     fflush(stdout);
 
     // (4) Copy the result data from the device memory to the host memory
@@ -121,15 +123,13 @@ void basicSgemm_d_1thread1element(int m, int k, int n, const float *A_h, const f
     CHECK(cudaMemcpy(C_h, C_d, sizeof(float) * m * n, cudaMemcpyDeviceToHost));
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (5) Free device memory
     CHECK(cudaFree(A_d));
     CHECK(cudaFree(B_d));
     CHECK(cudaFree(C_d));
-
-    return 0;
 }
 
 void basicSgemm_d_1thread1row(int m, int k, int n, const float *A_h, const float *B_h, float* C_h){
@@ -145,7 +145,7 @@ void basicSgemm_d_1thread1row(int m, int k, int n, const float *A_h, const float
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMalloc: %f s\n", endTime - startTime);
+    printf("    cudaMalloc:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (2) Copy data to device memory
@@ -155,19 +155,19 @@ void basicSgemm_d_1thread1row(int m, int k, int n, const float *A_h, const float
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (3) Call kernel to launch a grid of threads to perform the computation on GPU
-    dim3 gridDim(16, 1);
-    dim3 blockDim(ceil((float)m / blockDim.x), ceil((float)n / blockDim.y));
+    dim3 blockDim(1, BLOCK_SIZE);
+    dim3 gridDim(1, (m + blockDim.y - 1) / blockDim.y);
 
     startTime = myCPUTimer();
-    matrixMulKernel_1thread1element<<<gridDim, blockDim>>>(m, k, n, A_d, B_d, C_d);
+    matrixMulKernel_1thread1row<<<gridDim, blockDim>>>(m, k, n, A_d, B_d, C_d);
     CHECK(cudaDeviceSynchronize());
     endTime = myCPUTimer();
 
-    printf("    basicSgemm_d_1thread1row<<<(%d, %d, %d),(%d, %d, %d)>>>: %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
+    printf("    matrixMulKernel_1thread1row<<<(%d, %d, %d),(%d, %d, %d)>>>:                          %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
     fflush(stdout);
 
     // (4) Copy the result data from the device memory to the host memory
@@ -175,15 +175,13 @@ void basicSgemm_d_1thread1row(int m, int k, int n, const float *A_h, const float
     CHECK(cudaMemcpy(C_h, C_d, sizeof(float) * m * n, cudaMemcpyDeviceToHost));
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (5) Free device memory
     CHECK(cudaFree(A_d));
     CHECK(cudaFree(B_d));
     CHECK(cudaFree(C_d));
-
-    return 0;
 }
 
 void basicSgemm_d_1thread1column(int m, int k, int n, const float *A_h, const float *B_h, float* C_h){
@@ -199,7 +197,7 @@ void basicSgemm_d_1thread1column(int m, int k, int n, const float *A_h, const fl
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMalloc: %f s\n", endTime - startTime);
+    printf("    cudaMalloc:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (2) Copy data to device memory
@@ -209,19 +207,19 @@ void basicSgemm_d_1thread1column(int m, int k, int n, const float *A_h, const fl
     cudaDeviceSynchronize();
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (3) Call kernel to launch a grid of threads to perform the computation on GPU
-    dim3 gridDim(16, 1);
-    dim3 blockDim(ceil((float)m / blockDim.x), ceil((float)n / blockDim.y));
+    dim3 blockDim(BLOCK_SIZE, 1);
+    dim3 gridDim((n + blockDim.y - 1) / blockDim.y, 1);
 
     startTime = myCPUTimer();
-    matrixMulKernel_1thread1element<<<gridDim, blockDim>>>(m, k, n, A_d, B_d, C_d);
+    matrixMulKernel_1thread1column<<<gridDim, blockDim>>>(m, k, n, A_d, B_d, C_d);
     CHECK(cudaDeviceSynchronize());
     endTime = myCPUTimer();
 
-    printf("    basicSgemm_d_1thread1column<<<(%d, %d, %d),(%d, %d, %d)>>>: %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
+    printf("    matrixMulKernel_1thread1column<<<(%d, %d, %d),(%d, %d, %d)>>>:                     %f s\n", gridDim.x, gridDim.y, gridDim.z, blockDim.x, blockDim.y, blockDim.z, endTime - startTime);
     fflush(stdout);
 
     // (4) Copy the result data from the device memory to the host memory
@@ -229,21 +227,22 @@ void basicSgemm_d_1thread1column(int m, int k, int n, const float *A_h, const fl
     CHECK(cudaMemcpy(C_h, C_d, sizeof(float) * m * n, cudaMemcpyDeviceToHost));
     endTime = myCPUTimer();
 
-    printf("    cudaMemcpy: %f s\n", endTime - startTime);
+    printf("    cudaMemcpy:                                                                      %f s\n", endTime - startTime);
     fflush(stdout);
 
     // (5) Free device memory
     CHECK(cudaFree(A_d));
     CHECK(cudaFree(B_d));
     CHECK(cudaFree(C_d));
-
-    return 0;
 }
 
 bool verify(float* CPU_Answer, float* GPU_Answer, unsigned int nRows, unsigned int nCols) {
     unsigned int total = nRows * nCols; 
+    const float tolerance = 1e-3;
+
     for (unsigned int i = 0; i < total; ++i) {
-        if (CPU_Answer[i] != GPU_Answer[i]) {
+        if (fabs(CPU_Answer[i] - GPU_Answer[i]) > tolerance) {
+            printf("%f != %f on %d \n", CPU_Answer[i], GPU_Answer[i], i);
             return false;
         }
     }
@@ -253,78 +252,78 @@ bool verify(float* CPU_Answer, float* GPU_Answer, unsigned int nRows, unsigned i
 
 int main( int argc, char** argv){
     // setting dimensions (m * k * n)
-    unsigned int m = argv[0];
-    unsigned int k = argv[1];
-    unsigned int n = argv[2];
+    unsigned int m = atoi(argv[1]);
+    unsigned int k = atoi(argv[2]);
+    unsigned int n = atoi(argv[3]);
 
     bool isVerified;
+    double startTime, endTime;
 
-    
+    // CPU setup
+    float* A_h = (float*) malloc(sizeof(float) * m * k);  
+    float* B_h = (float*) malloc(sizeof(float) * k * n);  
+    float* C_h = (float*) calloc(m * n, sizeof(float));  
+    float* CPU_Answer = (float*) calloc(m * n, sizeof(float)); 
+
     // generating random float-point values
-    float* A_h = (float*) malloc(sizeof(float) * m * k);
-    for(unsigned int i = 0; i < n; i++){
+    for(unsigned int i = 0; i < m * k; i++){ 
         A_h[i] = rand()%100 / 100.0;
     }
-    float* B_h = (float*) malloc(sizeof(float) * k * n);
-    for(unsigned int i = 0; i < n; i++){
+    for(unsigned int i = 0; i < k * n; i++){  
         B_h[i] = rand()%100 / 100.0;
     }
-    float* C_h = (float*) calloc(n, sizeof(float));
-    float* CPU_Answer = (float*) calloc(n, sizeof(float));
-
-
+    
     // CPU
     startTime = myCPUTimer();
-    basicSgemm_h(int m, int k, int n, const float *A_h, const float *B_h, float* C_h)
+    basicSgemm_h(m, k, n, A_h, B_h, C_h);
     endTime = myCPUTimer();
 
-    printf("basicSgemm_h (CPU): %f s\n\n", endTime -startTime);
+    printf("basicSgemm_h (CPU):                                                                  %f s\n\n", endTime -startTime);
     fflush(stdout);
 
-    CPU_Answer = C_h;
+    memcpy(CPU_Answer, C_h, sizeof(float) * m * n);
 
     // basicSgemm_d_1thread1element (GPU)
     startTime = myCPUTimer();
-    basicSgemm_d_1thread1element(int m, int k, int n, const float *A_h, const float *B_h, float* C_h)
+    basicSgemm_d_1thread1element(m, k, n, A_h, B_h, C_h);
     endTime = myCPUTimer();
 
-    printf("basicSgemm_d_1thread1element (GPU): %f s\n\n", endTime -startTime);
+    isVerified = verify(CPU_Answer, C_h, m, n);
+    printf("    Verification Results:                                                            %s\n", isVerified ? "SUCCESS" : "FAILED");
     fflush(stdout);
 
-    isVerified = verify(CPU_Answer, C_h, m, n);
-    printf("    verified: %d s\n\n", isVerified);
+    printf("basicSgemm_d_1thread1element (GPU):                                                  %f s\n\n", endTime -startTime);
+    fflush(stdout);
 
     // basicSgemm_d_1thread1column (GPU)
     startTime = myCPUTimer();
-    basicSgemm_d_1thread1column(int m, int k, int n, const float *A_h, const float *B_h, float* C_h)
+    basicSgemm_d_1thread1column(m, k, n, A_h, B_h, C_h);
     endTime = myCPUTimer();
 
-    printf("basicSgemm_d_1thread1column (GPU): %f s\n\n", endTime -startTime);
+    isVerified = verify(CPU_Answer, C_h, m, n);
+    printf("    Verified Results:                                                                %s\n", isVerified ? "SUCCESS" : "FAILED");
     fflush(stdout);
 
-    isVerified = verify(CPU_Answer, C_h, m, n);
-    printf("    verified: %d s\n\n", isVerified);
+    printf("basicSgemm_d_1thread1column (GPU):                                                   %f s\n\n", endTime -startTime);
+    fflush(stdout);
 
     // basicSgemm_d_1thread1row (GPU)
     startTime = myCPUTimer();
-    basicSgemm_d_1thread1row(int m, int k, int n, const float *A_h, const float *B_h, float* C_h){
+    basicSgemm_d_1thread1row(m, k, n, A_h, B_h, C_h);
     endTime = myCPUTimer();
 
-    printf("basicSgemm_d_1thread1row (GPU): %f s\n\n", endTime -startTime);
+    isVerified = verify(CPU_Answer, C_h, m, n);
+    printf("    Verified Results:                                                                %s\n", isVerified ? "SUCCESS" : "FAILED");
     fflush(stdout);
 
-    isVerified = verify(CPU_Answer, C_h, m, n);
-    printf("    verified: %d s\n\n", isVerified);
-
+    printf("basicSgemm_d_1thread1row (GPU):                                                      %f s\n\n", endTime -startTime);
+    fflush(stdout);
 
     free(A_h);
     free(B_h);
     free(C_h);
 
     free(CPU_Answer);
-    free(isVerified);
-    free(startTime);
-    free(endTime);
 
     return 0;
 }
